4cb39b17509be5e556b27801e5002958ff33fd99
sohila558 <sohailat.441@gmail.com>
2025-08-06 22:35:31 +0300

    commit

---
+++ Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentADV01
+{
+    internal static class Helper
+    {
+        public static void SWAP(ref int X, ref int Z)
+        {
+            int Temp = X;
+            X = Z; 
+            Z = Temp;
+        }
+        public static void SWAP(ref double X, ref double Z)
+        {
+            double Temp = X;
+            X = Z;
+            Z = Temp;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Generics :
+            // C# feature 2005 C# 2.0
+            // Before 2005 Class Object
+
+            // Function SWAP
+
+            //int A = 4, B = 5;
+
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+
+            //Console.WriteLine("********** After SWAP ***********");
+
+            //Helper.SWAP( ref A, ref B ); // Passing By ref
+
+            //Console.WriteLine($"A: {A}");
+            //Console.WriteLine($"B: {B}");
+
+
+            //double L = 1.5, K = 2.6;
+
+            //Console.WriteLine($"A: {L}");
+            //Console.WriteLine($"B: {K}");
+
+            //Console.WriteLine("********** After SWAP ***********");
+
+            //Helper.SWAP(ref L, ref K); // Passing By ref
+
+            //Console.WriteLine($"A: {L}");
+            //Console.WriteLine($"B: {K}");
+
+
+
+
+
         }
     }
 }
